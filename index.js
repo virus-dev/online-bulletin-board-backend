b1a1432a180f001cd278138c328d7bfacf014a0b
Nikita <nikita-pashin12@yandex.ru>
2022-04-08 08:20:58 +0500

    fix

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ const app = express();
 app.use(cors());
 app.use(fileUpload({}));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', router);
 
 const start = async () => {
