3c143c7ff1e7c8b6562b6e8dfe8c84247a8f3a41
Nikita <nikita-pashin12@yandex.ru>
2022-04-15 13:13:16 +0500

    fix

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  req.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+  res.header('strict-origin-when-cross-origin', 'origin')
   next();
 });
 
